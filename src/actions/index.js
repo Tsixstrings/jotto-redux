09b4710e01ec760fbfe31b55ac628a39b81780a1
CTS\949635 <949635@cognizant.com>
2021-06-17 13:39:15 +0200

    - CORRECT_GUESS action creator and test

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,14 @@
 import axios from "axios";
 
+export const actionTypes = {
+  CORRECT_GUESS: "CORRECT_GUESS",
+};
+
+export function correctGuess() {
+  return {type: actionTypes.CORRECT_GUESS};
+}
+
 export const getSecretWord = () => {
-    // TODO: Write actual action in REDUX
+  // TODO: Write actual action in REDUX
   return axios.get("http://localhost:3030").then((response) => response.data);
 };
